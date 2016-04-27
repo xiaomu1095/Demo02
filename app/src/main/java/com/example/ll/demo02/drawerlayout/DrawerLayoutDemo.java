5ee5294387ac307a4e4dfb92881a5f30dac7a93a
xiaomu <lilin1095@gmail.com>
2016-04-27 17:04:53 +0800

    Zxing removed

--- app/src/main/java/com/example/ll/demo02/drawerlayout/DrawerLayoutDemo.java
+++ app/src/main/java/com/example/ll/demo02/drawerlayout/DrawerLayoutDemo.java
@@ -2,9 +2,8 @@ package com.example.ll.demo02.drawerlayout;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.ll.demo02.R;
 
@@ -39,10 +39,31 @@ public class DrawerLayoutDemo extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        DrawerLayout.DrawerListener drawerListener = new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        };
         if (drawer != null){
-            drawer.setDrawerListener(toggle);
+            drawer.addDrawerListener(drawerListener);
         }
-        toggle.syncState();
+//        toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         if (navigationView != null){
